3f2be7025742bf80116d9882479a8ab63458e1d7
Colin White <clwhite814@gmail.com>
2021-05-08 06:55:33 -0500

    wip: creating order stored procedure

---
+++ dbo/Stored Procedures/spOrders_GetById.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spOrders_GetById]
+	@id int
+AS
+begin
+	select 
+	[Id], [OrderName], [OrderDate], [FoodId], [Qauntity], [Total ] 
+	from dbo.[Order]
+	where Id = @id;
+end
---
+++ dbo/Stored Procedures/spOrders_Insert.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spOrders_Insert]
+	@orderName int = 0,
+	@orderDate int,
+	@FoodId int,
+	@Quantity int,
+	@Total money,
+	@Id int output
+AS
+	SELECT @param1, @param2
+RETURN 0
